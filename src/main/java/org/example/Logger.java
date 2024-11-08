3df3fe9f912c35ad8f2f6be87da5651f8043f122
Franco Disabato <francuster1@gmail.com>
2024-11-08 01:09:59 -0300

    implements observer

--- src/main/java/org/example/Logger.java
+++ src/main/java/org/example/Logger.java
@@ -1,18 +1,36 @@
 package org.example;
 
+import classroom.notifier.ClassroomNotifier;
+import classroom.notifier.entity.Observable;
+import classroom.notifier.implement.Observer;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
-public class Logger{
+public class Logger implements Observer<Map<String,String>> {
 
     private final String FILE_PATH = "historial.txt";
 
+    public Logger(ClassroomNotifier classroomNotifier) {
+        classroomNotifier.getAdministradorMaterias().addObserver(this);
+    }
 
-    private void writeToFile( String text) {
+    private void writeToFile(String text) {
         try (FileWriter writer = new FileWriter(FILE_PATH, true)) { // 'true' enables append mode
             writer.write(text + System.lineSeparator()); // Adds a newline after each message
         } catch (IOException e) {
             System.err.println("Error writing to file: " + e.getMessage());
         }
+    }
+
+    @Override
+    public void update(Observable<Map<String, String>> observable, Map<String, String> arg) {
+
+        arg.forEach((materia, aula) -> {
+            String mensaje = String.format("Aula: %s , materia: %s", aula, materia);
+            writeToFile(mensaje);
+            System.out.println(mensaje);
+        });
     }
 }
