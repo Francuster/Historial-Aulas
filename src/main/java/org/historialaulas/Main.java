7af1c3dcd7c7251709c009589b23e1fe2d37dfb8
Franco Disabato <francuster1@gmail.com>
2024-11-08 00:30:00 -0300

    Update Main.java

--- src/main/java/org/historialaulas/Main.java
+++ src/main/java/org/historialaulas/Main.java
@@ -8,17 +8,10 @@ import java.io.IOException;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-    private static final String FILE_PATH = "historial.txt";
 
     public static void main(String[] args) {
 
     }
 
-    private static void appendToFile(String filePath, String text) {
-        try (FileWriter writer = new FileWriter(filePath, true)) { // 'true' enables append mode
-            writer.write(text + System.lineSeparator()); // Adds a newline after each message
-        } catch (IOException e) {
-            System.err.println("Error writing to file: " + e.getMessage());
-        }
-    }
+
 }
